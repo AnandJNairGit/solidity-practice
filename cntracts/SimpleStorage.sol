1bb9af75d3d694ac35bec7ae008472ee68acc855
AnandJNairGit <anandjnairit@gmail.com>
2022-12-02 23:49:16 +0530

    create a store function

--- cntracts/SimpleStorage.sol
+++ cntracts/SimpleStorage.sol
@@ -2,4 +2,8 @@ pragma solidity 0.8.7; //this is a comment!
 
 contract SimpleStorage{
     uint256 myNumber;
+    
+    function store(unit256 _myNumber) public {
+        myNumber = _myNumber;
+    }
 }
